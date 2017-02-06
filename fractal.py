f1e21ef5e68f33332393f7a6e54e0a50f853e6d8
Belsebot <belse.bubba@gmail.com>
2017-02-06 19:26:55 +0200

    Update fractal.py

--- fractal.py
+++ fractal.py
@@ -2,59 +2,58 @@ from PIL import Image
 
 data = ""
 
-a=500
-pile=[[0 for x in range(a)]for y in range(a)]
+a=500							#picture size 500x500 pixel
+pile=[[0 for x in range(a)]for y in range(a)]		#2d array
 
 #piles
-pile[250][250]=50000
+pile[250][250]=50000					#puts 50000 pile in center of the screen
 
+loop=True						#set loop true
+count=0							#counter set to zero
 
-loop=True
-count=0
-
-while loop:
-	loop=False
-	for x in range(a):
+while loop:						#loop 
+	loop=False					#set loop false if picture is ready then loop will stops
+	for x in range(a):				
 		for y in range(a):
-			if pile[x][y] > 3:
-				if pile[x][y] % 4 == 0:
-					b=pile[x][y]/4
-					pile[x][y]-=pile[x][y]
-					if x<a-1:
-						pile[x+1][y]+=b
-					if x>0:
-						pile[x-1][y]+=b
-					if y<a-1:
-						pile[x][y+1]+=b
-					if y>0:
-						pile[x][y-1]+=b
+			if pile[x][y] > 3:				#if there is pile which is bigger than 3
+				if pile[x][y] % 4 == 0:			#check if pile can be divided by 4
+					b=pile[x][y]/4			#divide pile size by 4
+					pile[x][y]-=pile[x][y]		#and remove points from current pixel
+					if x<a-1:			#check border
+						pile[x+1][y]+=b		#distribute points to adjanced pixel
+					if x>0:				#check border 
+						pile[x-1][y]+=b		#distribute points to adjanced pixel
+					if y<a-1:			#check border
+						pile[x][y+1]+=b		#distribute points to adjanced pixel
+					if y>0:				#check border
+						pile[x][y-1]+=b		#distribute points to adjanced pixel
 
-				else:
-					pile[x][y]-=4
-					if x<a-1:
-						pile[x+1][y]+=1
+				else:					#if point is not divible by 4
+					pile[x][y]-=4			#minus 4 from pile
+					if x<a-1:			#and same as above but only adding 1 point to
+						pile[x+1][y]+=1		#adjanced pixel
 					if x>0:
 						pile[x-1][y]+=1
 					if y<a-1:
 						pile[x][y+1]+=1
 					if y>0:
 						pile[x][y-1]+=1
-				loop=True
-	count+=1
+				loop=True				#set loop to true if points have been tranfered
+	count+=1							#add counter
 
 
-for x in range(a):
+for x in range(a):							#picture making
 	for y in range(a):
-		if pile[x][y]==3:
-			data+=chr(255)+chr(0)+chr(0)
-		if pile[x][y]==2:
-			data+=chr(0)+chr(255)+chr(0)
-		if pile[x][y]==1:
-			data+=chr(0)+chr(0)+chr(255)
-		if pile[x][y]==0:
-			data+=chr(0)+chr(0)+chr(0)
+		if pile[x][y]==3:					#if pixel value is 3
+			data+=chr(255)+chr(0)+chr(0)			#then put pixel color to red
+		if pile[x][y]==2:					#if pixel value is 2
+			data+=chr(0)+chr(255)+chr(0)			#then put pixel color to green
+		if pile[x][y]==1:					#if pixel value is 1
+			data+=chr(0)+chr(0)+chr(255)			#then put pixel color to blue
+		if pile[x][y]==0:					#if pixel value is 0
+			data+=chr(0)+chr(0)+chr(0)			#then put pixel color to black
 
-im=Image.frombytes("RGB",(len(pile),len(pile[0])),data)
-im.save("Fractal.png","PNG")
-print "%d looppia loppu" % count
+im=Image.frombytes("RGB",(len(pile),len(pile[0])),data)			#create image
+im.save("Fractal.png","PNG")						#save image
+print "%d loops end" % count					#show how many loops had to make
 
